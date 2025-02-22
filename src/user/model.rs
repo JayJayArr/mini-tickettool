8a7f8e238285efebdc3fe49fb3672fc752a27e95
JayJayArr <JayArr@outlook.de>
2025-02-22 18:09:16 +0100

    impl User Repo

--- src/user/model.rs
+++ src/user/model.rs
@@ -1,10 +1,14 @@
+#[derive(Clone)]
 pub struct User {
-    user_id: UserId,
-    username: Username,
+    pub user_id: UserId,
+    pub username: Username,
 }
 
 pub struct UserDraft {
     pub username: Username,
 }
-pub struct UserId(u32);
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct UserId(pub u32);
+#[derive(Clone)]
 pub struct Username(String);
--- src/user/user_service.rs
+++ src/user/user_service.rs
@@ -3,11 +3,11 @@ use std::collections::HashMap;
 use super::model::{User, UserDraft, UserId};
 
 trait UserRepository {
-    async fn get_userss(&self) -> Vec<User>;
-    async fn get_user_by_id(&self, ticket_id: &UserId) -> Option<&User>;
-    async fn getmut_user_by_id(&mut self, ticket_id: &UserId) -> Option<&mut User>;
-    async fn create_user(&mut self, create_ticket: UserDraft) -> User;
-    async fn delete_user(&mut self, ticket_id: &UserId) -> Option<User>;
+    async fn get_users(&self) -> Vec<User>;
+    async fn get_user_by_id(&self, user_id: &UserId) -> Option<&User>;
+    async fn getmut_user_by_id(&mut self, user_id: &UserId) -> Option<&mut User>;
+    async fn create_user(&mut self, create_user: UserDraft) -> User;
+    async fn delete_user(&mut self, user_id: &UserId) -> Option<User>;
 }
 
 struct InMemUserRepository {
@@ -21,5 +21,28 @@ impl InMemUserRepository {
             users: HashMap::new(),
             counter: 1,
         }
+    }
+}
+
+impl UserRepository for InMemUserRepository {
+    async fn get_users(&self) -> Vec<User> {
+        self.users.values().cloned().collect()
+    }
+    async fn get_user_by_id(&self, user_id: &UserId) -> Option<&User> {
+        self.users.get(user_id)
+    }
+    async fn getmut_user_by_id(&mut self, user_id: &UserId) -> Option<&mut User> {
+        self.users.get_mut(user_id)
+    }
+    async fn create_user(&mut self, create_user: UserDraft) -> User {
+        self.counter += 1;
+        let user = User {
+            user_id: UserId(self.counter),
+            username: create_user.username,
+        };
+        self.users.insert(UserId(self.counter), user).unwrap()
+    }
+    async fn delete_user(&mut self, user_id: &UserId) -> Option<User> {
+        self.users.remove(user_id)
     }
 }
